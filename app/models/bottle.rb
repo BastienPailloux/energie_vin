7e158848606de6f15e0458eaf11a328d9d216f45
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2023-09-09 14:27:18 +0200

    validations

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -3,7 +3,7 @@ class Bottle < ApplicationRecord
   has_many :sells, dependent: :destroy
 
   validates :type, presence: true, inclusion: { in: %w(red pink white champagne) }
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :year }
   validates :property, presence: true
   validates :year, presence: true, numericality: { less_than_or_equal_to: Date.today.year }
 end
--- app/models/website.rb
+++ app/models/website.rb
@@ -1,3 +1,6 @@
 class Website < ApplicationRecord
   has_many :sells, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true
 end
