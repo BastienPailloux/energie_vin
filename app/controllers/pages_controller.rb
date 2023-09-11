dc111b4118c8a6a9cba49521aa260513e2852ff5
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2023-09-11 15:13:50 +0200

    banner

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @bottles = Bottle.all.order_by_rate
+    @min_price = 0
+    @max_price = 2000
+    if params[:minPrice].present? || params[:maxPrice].present?
+      @bottles = @bottles.select { |bottle| bottle.price.between?(params[:minPrice].to_i, params[:maxPrice].to_i) }
+      @min_price = params[:minPrice].to_i
+      @max_price = params[:maxPrice].to_i
+    end
   end
 end
